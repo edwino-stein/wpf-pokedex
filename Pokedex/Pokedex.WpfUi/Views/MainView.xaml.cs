fa0a868f54c6cf425c06d1ee5e5e7ce8b74d97ae
Edwino Stein <edwino.stein@gmail.com>
2021-04-26 11:22:07 -0400

    Add and setup the MvvmCross for WPF v7.1.2 in Pokedex.WpfUi project

---
+++ Pokedex/Pokedex.WpfUi/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace Pokedex.WpfUi.Views
+{
+    /// <summary>
+    /// Interação lógica para MainView.xam
+    /// </summary>
+    public partial class MainView : MvxWpfView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
